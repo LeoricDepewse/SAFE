7b56d6ba2754412f0b102a90f002a73a74e0520d
William Matthew Amy <Peacekeeper428@gmail.com>
2018-06-16 03:49:29 +0100

    connection and select database functions

--- database.cpp
+++ database.cpp
@@ -1,1 +1,56 @@
 #include "database.h"
+
+using namespace Database;
+bool connect()
+{
+    //connection reset
+    if(connected)
+        disconnect();
+
+    db = QSqlDatabase::addDatabase("QMYSQL");
+        db.setHostName(host);
+        db.setDatabaseName(DBNAME);
+        db.setUserName(user);
+        db.setPassword(pass);
+
+    connected = db.open();
+    if(connected)
+        return true;
+    else throw db.lastError().text();
+}
+
+void disconnect()
+{
+    db.close();
+    db.removeDatabase(db.connectionName());
+    connected = false;
+}
+
+QSqlQuery* select(std::string table)
+{
+    if(!connected)
+        return nullptr;
+
+    QSqlQuery* query = new QSqlQuery(db);
+    query->prepare("SELECT * FROM :table");
+        query->bindValue(":table", QString::fromStdString(table));
+
+    if(query->exec())
+        return query;
+    else throw query->lastError().text();
+}
+
+QSqlQuery *select(std::string table, std::string name)
+{
+    if(!connected)
+        return nullptr;
+
+    QSqlQuery* query = new QSqlQuery(db);
+    query->prepare("SELECT * FROM :table WHERE name = :name");
+        query->bindValue(":table", QString::fromStdString(table));
+        query->bindValue(":name", QString::fromStdString(name));
+
+    if(query->exec())
+        return query;
+    else throw query->lastError().text();
+}
--- database.h
+++ database.h
@@ -1,24 +1,29 @@
 #ifndef DATABASE_H
 #define DATABASE_H
 #include <QSqlDatabase>
+#include <QSqlQuery>
+#include <QSqlError>
+#include <QVariant>
 #include <map>
 #include <string>
 
 #define DBNAME "SAFE"
 typedef std::map<std::string, std::string> MySQLData;
 
-using namespace Database;
-static bool connect();
-static void disconnect();
-static QSqlQuery select(std::string table, std::string name = "*");
-static bool insert(std::string table, MySQLData data);
-static bool update(std::string table, MySQLData data);
-static bool erase(std::string table, std::string name);
+namespace Database
+{
+    static bool connect();
+    static void disconnect();
+    static QSqlQuery* select(std::string table);
+    static QSqlQuery* select(std::string table, std::string name);
+    static bool insert(std::string table, MySQLData data);
+    static bool update(std::string table, MySQLData data);
+    static bool erase(std::string table, std::string name);
 
-static bool connected;
-static QSqlDatabase db;
-static QString host = "localhost";
-static QString user = "root";
-static QString pass = "";
-
+    static bool connected;
+    static QSqlDatabase db;
+    static QString host = "localhost";
+    static QString user = "root";
+    static QString pass = "";
+}
 #endif // DATABASE_H
